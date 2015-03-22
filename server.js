79e73c5b565f871e5aa1379bd9e72f9811eb820c
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-22 11:05:11 +0800

    Output sample html

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
 
 http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+	fs.readFile('./index.html', function readFile (err, html) {
+		if(err) {
+			throw err;
+		}
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.end(html);
+	});
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
