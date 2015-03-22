86d2127e1ce29ded7136f0a4bf609b91feb85191
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-22 11:15:33 +0800

    Add sample routing

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	url = require('url');
 
 http.createServer(function (request, response) {
-	fs.readFile('./index.html', function readFile (err, html) {
-		if(err) {
-			throw err;
-		}
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.end(html);
-	});
+	var route = url.parse(request.url);
+
+	switch(route.path) {
+		case '/':
+			fs.readFile('./index.html', function readFile (err, html) {
+				if(err) {
+					throw err;
+				}
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.end(html);
+			});
+			break;
+		default:
+			response.writeHead(404, {'Content-Type': 'text/text'});
+			response.end('404 page not found');
+	}
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
